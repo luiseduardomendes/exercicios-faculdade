363e9dab98192a65e0834207ee27059334030e4d
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-09-07 14:24:52 -0300

    Update ex003.c

--- avaliacoes/semana005/ex003.c
+++ avaliacoes/semana005/ex003.c
@@ -1,6 +1,25 @@
+// Programa multiplataforma (não utiliza chamadas de sistema)
+
+/* A persistência multiplicativa de um número inteiro é o número de vezes que os dígitos
+ * deste número devem ser multiplicados, para que o resultado chegue a um único dígito
+ * (um número entre 0 e 9). Você deve fazer um programa que lê um valor inteiro n. Depois
+ * disso, o programa deve informar qual é a persistência multiplicativa do número n
+ * informado e qual foi o dígito resultante. Note que não sabemos o número de dígitos
+ * do número n informado.
+ * Por exemplo, se o usuário informar o número 6423, a persistência multiplicativa é 3 e o
+ * dígito resultante é 6, porque:
+ * 6*4*2*3=144
+ * 1*4*4=16
+ * 1*6=6
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+// função para definir o numero de letras de um valor
+int tamanho_numero(int num);
+
 int main()
 {
     int num, tamanho, num_auxiliar, i, resposta = 0, aux =1;
@@ -25,7 +44,7 @@ int main()
     return 0;
 }
 
-int tamanho_numero(int num){
+int tamanho_numero(int num) {
     int tamanho_num;
     tamanho_num = 1;
     while (num >= 10) {
