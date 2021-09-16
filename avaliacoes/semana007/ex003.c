ea9ac9b3550535f356ba493e205b9a3d9d6959ac
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-09-15 23:19:36 -0300

    update

---
+++ avaliacoes/semana007/ex003.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <locale.h>
+#include <time.h>
+#include <stdlib.h>
+#define N 10
+#define M 10
+#define BOMB -1
+
+int main(){
+    int matriz[M][N] = {0};
+    int interface[M][N] = {0};
+    int numx, numy, endOfGame = 0;
+    int total, i, j;
+
+    srand(time(NULL));   
+
+    scanf("%d",&total);
+
+    for (i = 0; i < total; i ++){
+        numx = ((float)rand()/RAND_MAX)*(N - 1);
+        numy = ((float)rand()/RAND_MAX)*(M - 1);
+        if(matriz[numy][numx] != BOMB) {
+            matriz[numy][numx] = BOMB;
+        }
+        else {
+            i --;
+        }
+    }
+    
+
+    for (i = 0; i < M; i ++) {
+        for (j = 0; j < N; j ++) {
+            if (matriz[i][j] != BOMB){
+                if(matriz [i - 1][j] == BOMB)
+                    matriz[i][j] ++;
+                if(matriz [i - 1][j - 1] == BOMB)
+                    matriz[i][j] ++;
+                if(matriz [i - 1][j + 1] == BOMB)
+                    matriz[i][j] ++;
+
+                if(matriz [i][j - 1] == BOMB)
+                    matriz[i][j] ++;
+                if(matriz [i][j + 1] == BOMB)
+                    matriz[i][j] ++;
+
+                if(matriz [i + 1][j] == BOMB)
+                    matriz[i][j] ++;
+                if(matriz [i + 1][j - 1] == BOMB)
+                    matriz[i][j] ++;
+                if(matriz [i + 1][j + 1] == BOMB)
+                    matriz[i][j] ++;
+            }
+        }
+    }
+    do {
+        for (i = 0; i < M; i ++) {
+            for (j = 0; j < N; j ++) {
+                if (interface[i][j] == 1)
+                    if (matriz[i][j] == BOMB)
+                        printf (" #");
+                    else
+                        printf("%2d", matriz[i][j]);
+                else 
+                    printf(" #");
+            }
+            printf("\n");
+        }
+        scanf("%d %d", &numx, &numy);
+        interface[numy][numx] = 1;
+        
+            if (matriz[numy][numx] == 0) {
+                if (matriz[numy - 1][numx] == 0){
+                    interface [numy - 1][numx] = 1;
+                }
+                if (matriz[numy + 1][numx] == 0){
+                    interface [numy + 1][numx] = 1;
+                }
+                if (matriz[numy][numx - 1] == 0){
+                    interface [numy][numx - 1] = 1;
+                }
+                if (matriz[numy][numx + 1] == 0){
+                    interface [numy][numx + 1] = 1;
+                }
+
+            }
+        
+    } while (!(endOfGame));
+    return 0;
+}
