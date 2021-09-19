945d54855742c5ae4b14b9ff8d0ce7291655bdca
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-09-19 12:39:38 -0300

    create read id & read price

--- avaliacoes/semana007/ex001.c
+++ avaliacoes/semana007/ex001.c
@@ -1,19 +1,37 @@
 #include <string.h>
 #include <stdio.h>
-#define SIZE_NAME 51
-#define NUM_PROD 3
-#define NUM_BRANCH 3
+#include <locale.h>
+#define SIZENAME 51
+#define NUMPROD 3
+#define NUMBRANCH 3
 
 
 int main() {
-    char product_names[NUM_PROD][SIZE_NAME];
-    int i;
-    char name[SIZE_NAME];
+    char productNames[NUMPROD][SIZENAME];
+    int i, id[NUMPROD];
+    char name[SIZENAME];
+    float price[NUMPROD];
+    int unitSold[NUMPROD][NUMBRANCH];
 
-    for (i = 0; i < NUM_PROD; i ++) {
-        fgets(&name, SIZE_NAME, stdin);
-        strcpy(product_names[i], name);
+    for (i = 0; i < NUMPROD; i ++) {
+        printf("Insira o código do produto: ");
+        scanf("%d", &id[i]);
+        flushIn();
+        printf("Insira o nome do produto: ");
+        fgets(&name, SIZENAME, stdin);
+        strcpy(productNames[i], name);
+        printf("Insira o preço do produto: ");
+        scanf("%f", price[i]);
     }
 
+
+
     return 0;
+}
+
+void flushIn() {
+    int ch;
+    do {
+        ch = fgetc(stdin);
+    } while (ch != EOF || ch != '\0');
 }
