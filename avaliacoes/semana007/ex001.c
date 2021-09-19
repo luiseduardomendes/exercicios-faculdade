8060bb302bf927e0f768e7b547934e0b5c904a93
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-09-19 20:58:30 -0300

    create profit registerers

--- avaliacoes/semana007/ex001.c
+++ avaliacoes/semana007/ex001.c
@@ -14,6 +14,11 @@ void searchById(char productNames[NUMPROD][SIZENAME],
 void searchByName(char productNames[NUMPROD][SIZENAME], 
                 int id[NUMPROD], float price[NUMPROD]);
 
+char month[NUMMONTH][11] = {"Janeiro", "Fevereiro", "Marco", 
+                                "Abril", "Maio", "Junho", 
+                                "Julho", "Agosto", "Setembro", 
+                                "Outubro", "Novembro", "Dezembro"};                
+
 
 int main() {
     char productNames[NUMPROD][SIZENAME];
@@ -21,25 +26,20 @@ int main() {
     int id[NUMPROD];
     char name[SIZENAME];
     char searchName[SIZENAME];
-    char month[NUMMONTH][11] = {"Janeiro", "Fevereiro", "Março", 
-                                "Abril", "Maio", "Junho", 
-                                "Julho", "Agosto", "Setembro", 
-                                "Outubro", "Novembro", "Dezembro"};
     float price[NUMPROD];
     int unitSold[NUMPROD][NUMBRANCH][NUMMONTH];
-    int searchId, index;
-    int found, sumSold;
-    int comparison, option;
+    int option;
 
     setlocale(LC_ALL, "");
     srand(time(NULL));
 
     for (i = 0; i < NUMPROD; i ++) {
+        clearscreen();
         
         printf("Insira o código do produto %d: ", i + 1);
         scanf("%d", &id[i]);
         
-        __fpurge(stdin);
+        flushIn();
         printf("Insira o nome do produto %d: ", i + 1);
         fgets(&name, SIZENAME, stdin);
         for (k = 0; k < SIZENAME; k ++) {
@@ -57,11 +57,11 @@ int main() {
                 /*printf("Insira a quatidade vendida do produto %d na filial %d no mês de %s: ", 
                         i + 1, j + 1, month[k]);
                 scanf("%d", &unitSold[i][j][k]);*/
-                unitSold[i][j][k] = (1+(float)rand()/RAND_MAX)*(50-1);
+                unitSold[i][j][k] = ((float)rand()/RAND_MAX)*(5);
 
             }
         }
-        __fpurge(stdin);
+        flushIn();
     }
 
     do{
@@ -74,24 +74,30 @@ int main() {
         case 2:
             searchById(productNames, id, price, unitSold);
             break;
+        case 3:
+            profitByBranch(productNames, id, price, unitSold);
+            break;
+        case 4:
+            profitByBranchAndId(productNames, id, price, unitSold);
+            break;
         }
     } while (option != 0);
-    
-
-    
-
 
     return 0;
 }
+
 int mainMenu() {
     int option;
+    clearscreen();
     do{
         printf("[1] Realizar pesquisa por nome\n"
         "[2] Realizar pesquisa por código\n"
+        "[3] Ver lucro por filial no ano\n"
+        "[4] Lucro por filial de um produto\n"
         "[0] Encerrar Programa \n"
         "Sua opção: ");
         scanf("%d", &option);
-    } while (option > 2 || option < 0);
+    } while (option > 4 || option < 0);
     return option;
 }
 
@@ -102,10 +108,12 @@ void searchByName(char productNames[NUMPROD][SIZENAME],
     int index;
     int found;
     int comparison;
+    flushIn();
     do {    
         do {
+            clearscreen();
             found = 0;
-            printf("Informe o nome de um produto[0 para encerrar]: ");
+            printf("Informe o nome de um produto [0 para encerrar]: ");
             fgets(&searchName, SIZENAME, stdin);
             for ( k = 0; k < SIZENAME; k ++){
                 if (searchName[k] == '\n'){
@@ -131,6 +139,9 @@ void searchByName(char productNames[NUMPROD][SIZENAME],
                     printf("  Código                                                Nome     Preço\n");
                     printf("%8d %51s %9.2f\n\n", id[index], productNames[index], price[index]);
                 }
+                flushIn();
+                printf("\nEnter para continuar...\n");
+                fgetc(stdin);
             }
             else {
                 comparison = 0;
@@ -151,9 +162,10 @@ void searchById(char productNames[NUMPROD][SIZENAME],
     int searchId, index;
     int found, sumSold;
     do {
-        __fpurge(stdin);
-        printf("Informe um código de produto[0 para encerrar]: ");
-        __fpurge(stdin);
+        flushIn();
+        clearscreen();
+        printf("Informe um código de produto [0 para encerrar]: ");
+        flushIn();
         scanf("%d", &searchId);
 
         if (searchId != 0) {
@@ -178,6 +190,96 @@ void searchById(char productNames[NUMPROD][SIZENAME],
             else {
                 printf("Produto não encontrado!\n");
             }
+            flushIn();
+            printf("\nEnter para continuar...\n");
+            fgetc(stdin);
         }
     } while (searchId != 0);
+}
+
+void profitByBranch(char productNames[NUMPROD][SIZENAME], 
+                int id[NUMPROD], float price[NUMPROD], 
+                int unitSold[NUMPROD][NUMBRANCH][NUMMONTH]){
+    int i, j, k;
+    float profit, profitMonth;
+    clearscreen();
+
+
+    for (i = 0; i < NUMBRANCH; i ++) {
+        profit = 0;
+        for (j = 0; j < NUMMONTH; j ++ ) {
+            profitMonth = 0;
+            for (k = 0; k < NUMPROD; k ++) {
+                profitMonth += (price[k] * unitSold[k][i][j]); 
+            }
+            printf("%14s%14.2f\n", month[j], profitMonth);
+            profit += profitMonth;
+        }
+        printf("Filial %d \nLucro total: R$%.2f\n\n", i+1, profit);
+    }
+    flushIn();
+    printf("Enter para continuar...\n");
+    fgetc(stdin);
+}
+
+void profitByBranchAndId (char productNames[NUMPROD][SIZENAME], 
+                int id[NUMPROD], float price[NUMPROD], 
+                int unitSold[NUMPROD][NUMBRANCH][NUMMONTH]) {
+    int searchId, found, index;
+    int numberSold;
+
+    int i, j;
+    do {
+        flushIn();
+        clearscreen();
+        printf("Informe um código de produto [0 para encerrar]: ");
+        flushIn();
+        scanf("%d", &searchId);
+
+        if (searchId != 0) {
+            found = 0;
+
+            for(i = 0; i < NUMPROD; i ++) {
+                if (id[i] == searchId){
+                    found = 1;
+                    index = i;
+                }
+            }
+            if(found) {
+                for (i = 0; i < NUMBRANCH; i ++) {
+                    printf("Filial: %d\tCódigo: %d\tProduto: %s\n\n", i+1, id[index], productNames[index]);
+                    numberSold = 0;
+                    printf("\t    Mês     Unidades\tLucro\n");
+                    for (j = 0; j < NUMMONTH; j ++) {
+                        printf("%16s %8d%14.2f\n", month[j], 
+                        unitSold[index][i][j], unitSold[index][i][j] * price[index]);
+                        numberSold += unitSold[index][i][j];
+                    }
+                    printf("\n    Total anual: %8d %14.2f\n\n", numberSold, numberSold * price[index]);
+                }
+            }
+            else {
+                printf("Produto não encontrado!\n");
+            }
+            flushIn();
+            printf("\nEnter para continuar...\n");
+            fgetc(stdin);
+        }
+    } while (searchId != 0);
+}
+
+void flushIn() {
+    #ifdef WIN32
+        fflush(stdin);
+    #elif _POSIX_C_SOURCE >= 199309L
+        __fpurge(stdin);
+    #endif
+}
+
+void clearscreen() {
+    #ifdef WIN32
+        system("cls");
+    #elif _POSIX_C_SOURCE >= 199309L
+        system("clear");
+    #endif
 }
