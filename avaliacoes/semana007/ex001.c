332f41ffb112c224d4b7f78d1ff1badea8714bc0
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-09-19 15:36:21 -0300

    create search by id

--- avaliacoes/semana007/ex001.c
+++ avaliacoes/semana007/ex001.c
@@ -1,9 +1,11 @@
 #include <string.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include <locale.h>
 #define SIZENAME 51
-#define NUMPROD 3
-#define NUMBRANCH 3
+#define NUMPROD 2
+#define NUMBRANCH 2
 #define NUMMONTH 12
 
 
@@ -18,25 +20,70 @@ int main() {
                                 "Outubro", "Novembro", "Dezembro"};
     float price[NUMPROD];
     int unitSold[NUMPROD][NUMBRANCH][NUMMONTH];
+    int searchId, index;
+    int found, sumSold;
+
+    setlocale(LC_ALL, "");
+    srand(time(NULL));
 
     for (i = 0; i < NUMPROD; i ++) {
+        
         printf("Insira o código do produto %d: ", i + 1);
         scanf("%d", &id[i]);
+        
         flushIn();
         printf("Insira o nome do produto %d: ", i + 1);
         fgets(&name, SIZENAME, stdin);
+        for (k = 0; k < SIZENAME; k ++) {
+            if (name[k] == '\n') {
+                name[k] = '\0';
+            }
+        }
         strcpy(productNames[i], name);
+        
+
         printf("Insira o preço do produto %d: ", i + 1);
         scanf("%f", &price[i]);
         for (j = 0; j < NUMBRANCH; j ++) {
             for (k = 0; k < NUMMONTH; k ++) {
-                printf("Insira a quatidade vendida do produto %d na filial %d no mês de %s: ", 
+                /*printf("Insira a quatidade vendida do produto %d na filial %d no mês de %s: ", 
                         i + 1, j + 1, month[k]);
-                scanf("%d", &unitSold[i][j][k]);
+                scanf("%d", &unitSold[i][j][k]);*/
+                unitSold[i][j][k] = (1+(float)rand()/RAND_MAX)*(50-1);
+
             }
         }
     }
 
+    do {
+        printf("Informe um código de produto[0 para encerrar]: ");
+        flushIn();
+        scanf("%d", &searchId);
+
+        if (searchId != 0) {
+            found = 0;
+
+            for(i = 0; i < NUMPROD; i ++) {
+                if (id[i] == searchId){
+                    found = 1;
+                    index = i;
+                }
+            }
+            sumSold = 0;
+            if (found){
+                for (i = 0; i < NUMBRANCH; i ++){
+                    for (j = 0; j < NUMMONTH; j ++){
+                        sumSold += unitSold[index][i][j];
+                    }
+                }
+                printf("  Código                                                Nome     Preço Vendidos\n");
+                printf("%8d %51s %9.2f %8d\n\n", id[index], productNames[index], price[index], sumSold);
+            }
+            else {
+                printf("Produto não encontrado!\n");
+            }
+        }
+    } while (searchId != 0);
 
 
     return 0;
