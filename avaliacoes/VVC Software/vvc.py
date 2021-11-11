75caa19f222b6d85f622646a3441b733254722c1
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-11-11 10:25:46 -0300

    update

--- avaliacoes/VVC Software/vvc.py
+++ avaliacoes/VVC Software/vvc.py
@@ -4,41 +4,37 @@ import re
 
 VideoList = ['RaceHorsesC']
 
-AppList = ['nosatd4/', 'app_zeros_1_4_4/', 'app_zeros_2_4_4/', 'app_zeros_3_4_4/', 'app_zeros_4_4_4/',
-           'app_zeros_5_4_4/', 'app_zeros_6_4_4/', 'app_zeros_7_4_4/', 'app_zeros_8_4_4/', 'app_zeros_9_4_4/',
-           'app_zeros_10_4_4/', 'app_zeros_11_4_4/', 'app_zeros_12_4_4/', 'app_zeros_13_4_4/', 'app_zeros_14_4_4/',
-           'app_zeros_15_4_4/']
+nameCfg = ['low_delay_', 'random_access', 'all_intra']
 QPList = ['22', '27', '32', '37']
+
 NFrames = sys.argv[1]
 
-cfgList = ['encoder_lowdelay_vtm.cfg', 'encoder_randomaccess_vtm.cfg', 'encoder_intra_.cfg']
+cfgList = ['encoder_lowdelay_vtm.cfg', 'encoder_randomaccess_vtm.cfg', 'encoder_intra_vtm.cfg']
 
 for video in VideoList:
-    for app in AppList:
-        fd1 = open(app + 'result_' + video + '.txt', 'w')
-        for config in cfgList:
-            for qp in QPList:
+    for config in cfgList:
+        for qp in QPList:
+            fd1 = open('config_' + config + 'result_' + video + 'QP_' + qp + '.txt', 'w')
 
-                os.system(
-                    './' + app + 'Enc -c ../cfg/' + cfg + ' -c ../cfg/per-sequence/' + video + '.cfg --QP=' + qp + ' -f ' + NFrames + ' >> ' + app + 'report_' + video + '_' + qp + '.txt')
-                # perguntar para o professor
-                fd = open(app + 'report_' + video + '_' + qp + '.txt', 'r')
+            os.system(
+                './EncoderAppStatic -c ../cfg/' + config + ' -c ../cfg/per-sequence/' + video + '.cfg --QP=' + qp + ' -f ' + NFrames + ' >> ' + 'EncoderAppStatic' + 'report_' + video + '_' + qp + '.txt')
+            # perguntar para o professor
+            fd = open('config_' + config + 'result_' + video + 'QP_' + qp + '.txt', 'r')
+            l = fd.readline()
 
+            while 'SUMMARY' not in l:
                 l = fd.readline()
 
-                while 'SUMMARY' not in l:
-                    l = fd.readline()
+            l = fd.readline()
+            l = fd.readline()
 
-                l = fd.readline()
-                l = fd.readline()
+            infos = re.match(r'\s*\d*\s*a\s*(\d*.\d*)\s*(\d*.\d*).*', l, 0)
 
-                infos = re.match(r'\s*\d*\s*a\s*(\d*.\d*)\s*(\d*.\d*).*', l, 0)
-
-                fd1.write(qp + ',' + infos.group(1) + ',' + infos.group(2) + '\n')
+            fd1.write(qp + ',' + infos.group(1) + ',' + infos.group(2) + '\n')
 
         fd.close()
         fd1.close()
-
+    
 for video in VideoList:
     fd1 = open('correct/result_' + video + '.txt', 'w')
     for config in cfgList:
--- avaliacoes/semana014/ex003.c
+++ avaliacoes/semana014/ex003.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    char str[] = {"(9*(5-3))-(4/(9*8))/(8-13)"};
-
-    printf("%d", testaParenteses(str, 0, 0));
+    char str[] = {"(9*(5-3)-(4/(9*8))/(8-13"};
+    int num = 0;
+    printf("%d\n", testaParenteses(str, 0, 0));
     
 }
 
 int testaParenteses(char str[], int i, int aberto){
 
-    if (str[i] != '(' && str[i] != ')') 
-        i++;
-    aberto = 0;
+    if ((str[i] == '\0' || str[i] == '\n') && aberto == 0)
+        return 1;
+    else if (str[i] != '(' && str[i] != ')') 
+        testaParenteses(str, i+1, aberto);
 
-    else if (str[i] == ')') 
-        if (aberto) 
-            return 1;
+    else if (str[i] == ')') {
+        if (aberto > 0) {
+            return 1 * testaParenteses(str, i+1, aberto-1);
+        }
         else 
             return 0;
+    }
     
-    
-    else if (str[i] == '(') 
-        return testaParenteses(str, i+1, 1);
-    
+    else if (str[i] == '(') {   
+        return 1 * testaParenteses(str, i+1, aberto + 1);
+    }
 }
