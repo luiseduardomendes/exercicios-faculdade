522530c433ea4139ddfa766f8c391044b0863574
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-11-16 21:29:05 -0300

    Edubot Maze working

--- avaliacoes/Edubot/main.cpp
+++ avaliacoes/Edubot/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <math.h>
 #include "libs/EdubotLib.hpp"
 
 using namespace std;
@@ -20,50 +21,52 @@ int main(){
 	//try to connect on robot
 	if(edubotLib->connect()) {
 		wallDirect = RIGHT;
+		edubotLib->sleepMilliseconds(1000);
 		while (edubotLib->isConnected()) {
 			edubotLib->move(0.3);
-			flag = 1;
-			do{
-				edubotLib->sleepMilliseconds(2);
-				cout << "sonar meio " << edubotLib->getSonar(MID) << endl;
-				if (edubotLib->getSonar(MID) >= 0.25){
+			
+			
+			if (count == 0){
+				while(edubotLib->getSonar(MID) >= 0.1)
+					edubotLib->sleepMilliseconds(1);
+				
+				if (edubotLib->getSonar(RIGHT) > edubotLib->getSonar(LEFT)) {
+					rotateRight(&wallDirect, &edubotLib);
 					
-					 edubotLib->sleepMilliseconds(2);
-					 if (edubotLib->getSonar(wallDirect) > 0.5){
-					 	cout << "sonar " << edubotLib->getSonar(wallDirect) << endl;
-					 	if(wallDirect == RIGHT){
-						 	endOfWall(RIGHT, &edubotLib);
-						 	count ++;
-						 	flag = 0;
-						 }
-						 else{
-						 	endOfWall(LEFT, &edubotLib);
-						 	count --;
-						 	flag = 0;
-						 }
-					 }
-					 else{
-					 	edubotLib->sleepMilliseconds(2);
-					 }
-				}
-				else if (edubotLib->getSonar(MID) < 0.25){
-					if (edubotLib->getSonar(RIGHT) > edubotLib->getSonar(LEFT)) {
-						rotateRight(&wallDirect, &edubotLib);
-						count ++;
-						flag = 0;
-					}
-					else{			
-						rotateLeft(&wallDirect, &edubotLib);
-						count --;
-						flag = 0;
-					}
-				}
-				cout << "contador: " << count << endl;
-				if (count == 0){
-					edubotLib->disconnect();
+					wallDirect = LEFT;
+					count ++;
+					
 					
 				}
-			} while (flag);
+				else{			
+					rotateLeft(&wallDirect, &edubotLib);
+					wallDirect = RIGHT;
+					count --;
+					
+				}
+			}
+			edubotLib->move(0.3);
+			while(edubotLib->getSonar(MID) >= 0.25 && edubotLib->getSonar(wallDirect) <= 0.25)
+				edubotLib->sleepMilliseconds(1);
+			if(edubotLib->getSonar(MID) < 0.25){
+				if(edubotLib->getSonar(RIGHT) > edubotLib->getSonar(LEFT)){
+					rotateRight(&wallDirect, &edubotLib);
+					count ++;
+				}
+				else{
+					rotateLeft(&wallDirect, &edubotLib);
+					count --;
+				}
+				
+			}
+			else if (edubotLib->getSonar(wallDirect) > 0.25){
+				endOfWall(wallDirect, &edubotLib);
+				
+				
+			}
+			
+				
+			
 			
 			
 		}
@@ -89,15 +92,16 @@ void rotateLeft(int* wallDirect, EdubotLib **edubotLib){
 }
 
 void endOfWall(int direct, EdubotLib **edubotLib){
-	int flag = 0;
+	int flag = -1;
 	if (direct == RIGHT)
 		flag = 1;
 	
-	(*edubotLib)->sleepMilliseconds(750);
+	(*edubotLib)->sleepMilliseconds(950);
  	(*edubotLib)->stop();
  	(*edubotLib)->rotate(90 * flag);
  	(*edubotLib)->sleepMilliseconds(1500);
  	(*edubotLib)->move(0.3);
  	(*edubotLib)->sleepMilliseconds(1500);
+ 	
 }
 
