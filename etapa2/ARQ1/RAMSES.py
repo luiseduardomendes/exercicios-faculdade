4d1d481964caf5c9ef6d0d338b6e48dc3ac03163
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2022-02-13 22:57:39 -0300

    update

--- etapa2/ARQ1/RAMSES.py
+++ etapa2/ARQ1/RAMSES.py
@@ -3,86 +3,96 @@ from numpy import number
 
 from pandas import DataFrame
 
-for k in range (0,5):
-    v = []
-    vi = []
-    vn = []
+class new_test_ramses:
 
-    for i in range(0,15):
-        v.append(0);
-
-    v[7 + randint(0,7)] = 255
-
-    i = 0
-    while v[i] != 255:
-        v[i] = randint(0, 16) + randint(0,1) * 128
-        i += 1
-
-
-    i = 0
-    print("Vetor original: ")
-    while v[i] != 255 : 
-        print(f"{v[i]}  ", end='')
-        i += 1
-
-    print("\n");
-
-    i = 0
-    size_index_vec = 0
-    size_number_vec = 0
-    while v[i] != 255:
-        aux = v[i]
-        if aux >= 128:
-            aux = aux - 128;
-            vi.append(aux)
-            size_index_vec += 1
+    def __init__(self, name: str):
         
-        elif aux > 0 and aux <= 128 :
+        self.set_name(name)
         
-            vn.append(aux)
-            size_number_vec += 1
+        self.init_lists()
+
+        self.__generate_random_values__()
+
+        self.split_vector_index_and_number()
+
+        self.sum = self.set_sum()
+
+        self.generate_excel_file()
+
+    def set_name(self, name):
+        self.output_file = name
+
+    def init_lists(self):
+        self.v = []
+        self.vi = []
+        self.vn = []        
+
+        for i in range(0,15):
+            self.v.append(0);
+
+    def __generate_random_values__(self):
+
+        self.v[7 + randint(0,7)] = 255
+
+        i = 0
+        while self.v[i] != 255:
+            self.v[i] = randint(0, 16) + randint(0,1) * 128
+            i += 1
+
+    def split_vector_index_and_number(self):
+        i = 0
+        self.size_index_vec = 0
+        self.size_number_vec = 0
+        while self.v[i] != 255:
+            aux = self.v[i]
+            if aux >= 128:
+                aux = aux - 128;
+                self.vi.append(aux)
+                self.size_index_vec += 1
+            
+            elif aux > 0 and aux <= 128 :
+            
+                self.vn.append(aux)
+                self.size_number_vec += 1
+            
+            i += 1
         
-        i += 1
+    def generate_excel_file(self):
+        for i in range(self.size_number_vec,len(self.v)):
+            self.vn.append('')
+        for i in range(self.size_index_vec,len(self.v)):
+            self.vi.append('')
 
-    print("Vetor de indices: ");
-    for j in vi:
-        print(f"{j}  ", end='')
-    print("\n")
+        soma_vector = []
+        soma_vector.append(self.sum)
+        for i in range(1,len(self.v)):
+            soma_vector.append('')
 
-    print("Vetor de numeros: ");
-    for j in vn:
-        print(f"{j}  ", end='')
-    print("\n")
+        vectors = {
+            'original_vector': self.v,
+            'number_vector': self.vn,
+            'index_vector': self.vi,
+            'soma_total': soma_vector
+        }
+        
+        DataFrame(vectors).to_excel(excel_writer=self.output_file)
 
-    if size_number_vec < size_index_vec:
-        min_value_size = size_number_vec
-    else:
-        min_value_size = size_index_vec
+    def set_sum(self):
+        if self.size_number_vec < self.size_index_vec:
+            min_value_size = self.size_number_vec
+        else:
+            min_value_size = self.size_index_vec
 
-    soma = 0
-    for cont in range (0, min_value_size):
-        if vi[cont] < size_number_vec:
-            soma += vn[vi[cont]]
+        sum = 0
+        for cont in range (0, min_value_size):
+            if self.vi[cont] < self.size_number_vec:
+                sum += self.vn[self.vi[cont]]
 
-    print(f"soma final: {soma}\n")
+        return sum
 
-    for i in range(size_number_vec,len(v)):
-        vn.append('')
-    for i in range(size_index_vec,len(v)):
-        vi.append('')
-
-    soma_vector = []
-    soma_vector.append(soma)
-    for i in range(1,len(v)):
-        soma_vector.append('')
-
-    vectors = {
-        'original_vector': v,
-        'number_vector': vn,
-        'index_vector': vi,
-        'soma_total': soma_vector
-    }
-
-    excel_file = open(f'new_excel{k}.xlsx', 'w')
-    data_frame = DataFrame(vectors).to_excel(excel_writer=f'new_excel{k}.xlsx')
-    print(data_frame)
+    def show_vector(self, v: list):
+        for elem in v:
+            print(f"{elem}  ", end='')
+        
+for i in range(0,100):
+    generator_excel = new_test_ramses(f'excel_file{i}.xlsx')
